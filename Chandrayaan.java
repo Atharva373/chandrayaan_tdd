42c1c8e43676d72eceea23125905ed3dfb48bb76
Your Name~ <you@example.com>
2023-09-15 12:09:53 +0530

    Initial Commit

--- Chandrayaan.java
+++ Chandrayaan.java
@@ -8,6 +8,7 @@ public class Chandrayaan {
     private static final int S=1;
     private static final int E=2;
     private static final int W=3;
+
     private static final int U=4;
     private static final int D=5;
 
