f8823896bc7ecf6442a05097826d43aba1c86e6b
Your Name~ <you@example.com>
2023-09-15 12:23:28 +0530

    Function for rover to move forward

--- Chandrayaan.java
+++ Chandrayaan.java
@@ -4,12 +4,24 @@ import java.util.*;
 public class Chandrayaan {
     //Data Member Constants:
 
-    private static final int N=0;
-    private static final int S=1;
-    private static final int E=2;
-    private static final int W=3;
+    private static final int N=0;//Constant for North direction
+    private static final int S=1;//Constant for South direction
+    private static final int E=2;//Constant for East  direction
+    private static final int W=3;//Constant for West  direction
+    private static final int U=4;//Constant for Up    direction
+    private static final int D=5;//Constant for Down  direction
 
-    private static final int U=4;
-    private static final int D=5;
+    private static void goForward(int x,int y,int z,int direction){
+        int d=direction;
+
+        if(d==N) y++;
+        else if(d==S)y--;
+        else if(d==E)x++;
+        else if(d==W)x--;
+        else if(d==U)z++;
+        else z--;
+
+    }
+
 
 }
