886593f4e257d03e453ced6a20542afb268e1138
Your Name~ <you@example.com>
2023-09-15 11:56:18 +0530

    initial commit

---
+++ Chandrayaan.java
@@ -0,0 +1,14 @@
+package Chandrayaan;
+import java.util.*;
+
+public class Chandrayaan {
+    //Data Member Constants:
+
+    private static final int N=0;
+    private static final int S=1;
+    private static final int E=2;
+    private static final int W=3;
+    private static final int U=4;
+    private static final int D=5;
+
+}
